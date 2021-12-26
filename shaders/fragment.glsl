bf22bade9ca1e02566b7d67d4696c77196e8e71d
gre-v-el <myszkier.gabriel@gmail.com>
2021-12-26 22:56:42 +0100

    phong rendering

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -2,14 +2,22 @@
 uniform vec3 camPos;
 uniform vec4 rotation;
 uniform float depthMax;
+uniform vec3 lightDirection;
 
 // per-pixel variables
 varying vec2 UV;
 
 // consts
 const float PI = 3.141592653589793238;
+const float PROXIMITY_EPSILON = 0.0001;
+const float SHADOW_SOFTNESS = 0.2;
 
-
+const vec3 color = vec3(0.0, 0.6784, 0.3961);
+const float ambientMult = 0.3;
+const float diffuseMult = 0.8;
+const float specularMult = 0.2;
+const float roughness = 0.05;
+const float metallic = 0.;
 
 // sd primitives
 float sdSphere( vec3 p, float r ){
@@ -27,21 +35,86 @@ float sdRoundBox( vec3 p, vec3 b, float r )
 }
 float sdPlane( vec3 p, vec3 n, float h )
 {
-  // n must be normalized
   return dot(p,n) + h;
+}
+
+float sdMandelbulb(vec3 pos) {
+	int iter = 5;
+	float power = 8.;
+	float bailout = 2.5;
+
+	vec3 z = pos;
+	float dr = 1.0;
+	float r = 0.0;
+	for (int i = 0; i < iter; i++) {
+		r = length(z);
+		if (r>bailout) break;
+		
+		// convert to polar coordinates
+		float theta = acos(z.z/r);
+		float phi = atan(z.y,z.x);
+		dr =  pow( r, power-1.0)*power*dr + 1.0;
+		
+		// scale and rotate the point
+		float zr = pow( r,power);
+		theta = theta*power;
+		phi = phi*power;
+		
+		// convert back to cartesian coordinates
+		z = zr*vec3(sin(theta)*cos(phi), sin(phi)*sin(theta), cos(theta));
+		z+=pos;
+	}
+	return 0.5*log(r)*r/dr;
+}
+
+float sdMenger(vec3 p){
+
+	float d = sdBox(p,vec3(1.0));
+	vec4 res = vec4( d, 1.0, 0.0, 0.0 );
+
+	float s = 1.0;
+	for( int m=0; m<8; m++ )
+	{
+		vec3 a = mod( p*s, 2.0 )-1.0;
+		s *= 3.0;
+		vec3 r = abs(1.0 - 3.0*abs(a));
+
+		float da = max(r.x,r.y);
+		float db = max(r.y,r.z);
+		float dc = max(r.z,r.x);
+		float c = (min(da,min(db,dc))-1.0)/s;
+
+		if( c>d )
+		{
+			d = c;
+			res = vec4( d, 0.2*da*db*dc, (1.0+float(m))/4.0, 0.0 );
+		}
+	}
+
+	return res.x;
+}
+
+// operations
+float smin(float a, float b, float k)
+{
+    float res = exp2( -k*a ) + exp2( -k*b );
+    return -log2( res )/k;
+}
+float diff(float a, float b){
+	return max(a, -b);
 }
 
 // sd scene
 float sdScene(vec3 p){
-	return min(
-		sdRoundBox(p, vec3(0.2, 0.4, 0.3), 0.05),
-		sdBox(p + vec3(0., 0.6, 0.), vec3(1., 0.05, 1.)));
+
+	return min(min(sdMenger(p + vec3(1.5, 0., 0.)), sdMandelbulb(p - vec3(1.5, 0., 0.))),
+			sdBox(p + vec3(0., 1.5, 0.), vec3(6., 0.05, 4.)));
 }
 
 // sd utilities
 vec3 calcNormal( vec3 p ) // for function f(p)
 {
-    const float h = 0.0001; // replace by an appropriate value
+    const float h = PROXIMITY_EPSILON;
     const vec2 k = vec2(1,-1);
     return normalize( k.xyy*sdScene( p + k.xyy*h ) + 
                       k.yyx*sdScene( p + k.yyx*h ) + 
@@ -54,30 +127,74 @@ vec3 qtransform( vec4 q, vec3 v ){
 	return v + 2.0*cross(cross(v, q.xyz ) + q.w*v, q.xyz);
 } 
 
+
+
 void main() {
 
-	vec3 ray = normalize(vec3(UV, -0.7));
-	ray = qtransform(rotation, ray);
-	vec3 rayPos = camPos;
+	// main march
+	vec3 viewRayDirection = normalize(vec3(UV, -0.7));
+	viewRayDirection = qtransform(rotation, viewRayDirection);
+	vec3 viewRayPos = camPos;
 
-	float depth = 0.;
+	float viewRayDist;
+	float viewDepth = 0.;
 	for(int i = 0; i < 256; i++){
 
-		float t = sdScene(rayPos);
-		depth += t;
+		viewRayDist = sdScene(viewRayPos);
+		viewDepth += viewRayDist;
 
-		rayPos += t * ray;
+		viewRayPos += viewRayDist * viewRayDirection;
 		
-		if(t < 0.0001 || depth>depthMax) break;
+		if(viewRayDist < PROXIMITY_EPSILON || viewDepth>depthMax) break;
 	}
 
-	vec3 col = vec3(0.);
-	if(depth < depthMax){
-		vec3 normal = calcNormal(rayPos);
+	// after-hit things:
+	vec3 normal = calcNormal(viewRayPos);
 
-		//col = vec3(depth/depthMax);
-		//col = vec3(normal);
-		col = vec3(max(dot(vec3(0.5, 0.7, 0.6), normal), 0.) + 0.05);
+	// shadow march 
+	vec3 shadowRayDirection = lightDirection;
+	vec3 shadowRayPos = viewRayPos + normal*PROXIMITY_EPSILON*2.;
+
+	float shadowRayDist;
+	float shadowDepth = PROXIMITY_EPSILON;
+	float shadow = 1.;
+	for(int i = 0; i < 64; i++){
+
+		shadowRayDist = sdScene(shadowRayPos);
+
+		shadowRayPos += shadowRayDist * shadowRayDirection;
+		shadowDepth += shadowRayDist;
+
+		shadow = min(shadow, shadowRayDist/SHADOW_SOFTNESS/shadowDepth);
+
+		if(shadowDepth>2.*depthMax) break;
+	}
+	shadow = max(shadow, 0.);
+
+
+	// calculate color
+	vec3 col;
+	if(viewDepth < depthMax){ // scene
+		
+		// Phong lighting
+		float dotLightNormal = dot(lightDirection, normal);
+
+		float ambient = ambientMult;
+		float diffuse = diffuseMult * max(dotLightNormal, 0.);
+		float specular = shadow * min(specularMult*( pow(max(dot((2.*dotLightNormal*normal - lightDirection), -viewRayDirection), 0.), 1./roughness) ), 1.);
+
+		//float illumination = ambient + diffuse + specular;
+
+		// other
+		float fresnel = 1.-dot(normal, -viewRayDirection);
+
+		// final
+		col = specular + vec3(ambient + diffuse) * color * (0.2 + 0.8*shadow);
+	}
+	else{ // background
+		vec3 light = vec3(0.5098, 0.8196, 1.0);
+		vec3 dark = vec3(0.0, 0.3882, 0.6118);
+		col = (viewRayDirection.y*0.5 + 0.5)*(light-dark) + dark;
 	}
 
 
